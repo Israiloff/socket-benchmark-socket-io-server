94887b3afa16a84b8cc2377bcaccae4c026c5414
Israiloff <stardustindark@gmail.com>
2024-10-31 18:57:22 +0500

    initial commit;

---
+++ src/main/java/io/github/israiloff/socket_io_server/SocketIoServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.israiloff.socket_io_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketIoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketIoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/israiloff/socket_io_server/SocketIoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.israiloff.socket_io_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketIoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
