3ff1313c7636f3630e12cc8d2e9295a6f7af67c1
Israiloff <stardustindark@gmail.com>
2024-10-31 19:04:12 +0500

    fixed: package;

--- src/main/java/io/github/israiloff/socket/io/server/SocketIoServerApplication.java
+++ src/main/java/io/github/israiloff/socket/io/server/SocketIoServerApplication.java
@@ -1,4 +1,4 @@
-package io.github.israiloff.socket_io_server;
+package io.github.israiloff.socket.io.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SocketIoServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SocketIoServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SocketIoServerApplication.class, args);
+    }
 
 }
