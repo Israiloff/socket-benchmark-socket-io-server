05f1646bc2e2436e72dc3da6a78d65702e235554
Israiloff <stardustindark@gmail.com>
2024-10-31 19:05:13 +0500

    fixed: test package;

--- src/test/java/io/github/israiloff/socket/io/server/SocketIoServerApplicationTests.java
+++ src/test/java/io/github/israiloff/socket/io/server/SocketIoServerApplicationTests.java
@@ -1,4 +1,4 @@
-package io.github.israiloff.socket_io_server;
+package io.github.israiloff.socket.io.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SocketIoServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
