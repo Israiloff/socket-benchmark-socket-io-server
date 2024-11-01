d51def9db6482c8d10c96ad155c1ca4df558978a
Israiloff <stardustindark@gmail.com>
2024-11-01 17:27:29 +0500

    added: package scanning;

--- src/main/java/io/github/israiloff/socket/io/server/SocketIoServerApplication.java
+++ src/main/java/io/github/israiloff/socket/io/server/SocketIoServerApplication.java
@@ -3,7 +3,7 @@ package io.github.israiloff.socket.io.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "io.github.israiloff")
 public class SocketIoServerApplication {
 
     public static void main(String[] args) {
